d24b62eaec572b741eb3035d5993e55c9b8f0420
Christopher Teodoro <54965651+ChristopherTeodoro@users.noreply.github.com>
2020-02-25 21:20:05 -0600

    Updates

---
+++ precache-manifest.9aef2de89e5cfac69358b2f126a52326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cfa41db5b4d2bfdbd6efb24d66d712",
+    "url": "/%7BClickster%7D/index.html"
+  },
+  {
+    "revision": "6fa103ded223e7a843d7",
+    "url": "/%7BClickster%7D/static/css/main.7eedff02.chunk.css"
+  },
+  {
+    "revision": "2b28977e3b6df7765ab2",
+    "url": "/%7BClickster%7D/static/js/2.f0a6a870.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/%7BClickster%7D/static/js/2.f0a6a870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa103ded223e7a843d7",
+    "url": "/%7BClickster%7D/static/js/main.1c25693c.chunk.js"
+  },
+  {
+    "revision": "4bc57b6ce33b238049a6",
+    "url": "/%7BClickster%7D/static/js/runtime-main.45ee633a.js"
+  }
+]);
